48923d57ce80622a2189187fed4d71d3a17dce86
shashank karan <shashankkaran999@gmail.com>
2020-06-26 15:51:28 +0530

    yes

--- action.php
+++ action.php
@@ -8,6 +8,102 @@
 <body>
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+
+
+$servername ="localhost";
+$username = "root";
+$password = "";
+$dbname = "testdb";
+
+
+$con = mysqli_connect( $servername,$username ,$password, $dbname );
+
+if($con)
+{
+    echo " connected ";
+
+}
+else
+{
+
+    die("failed".mysql_connect_error()) ;
+
+}
+
+
+
+
+
+
+
+$sql = "SELECT * FROM `testtable`";
+
+
+
+
+
+
+echo "start <br> "  ;
+
+
+
+
+
+error_reporting(0);
+
+
+
+
+
+
+?> 
+
+
+
+
+
+<?php
+
+
+
+$sn = $_GET ['name'];
+$rl=$_GET ['roll'];
+$ag = $_GET ['age'];
+$cl=$_GET['class'];
+
+
+
+$query =  "insert('$sn','$rl','$ag','$cl')";
+$data = mysqli_query($con,$query) ;
+
+if($data)
+{
+echo "Data inserted into database " ;
+
+}
+else {
+    echo"fuck you<br>";
+}
+
+
+?>
+
+
+
 <?php
 
 echo "thank for submitting"
--- index.php
+++ index.php
@@ -7,92 +7,10 @@
 </head>
  <body style="  background:  hotpink    ; text-align:center;  ">
 
-<?php
-
-
-$servername ="localhost";
-$username = "root";
-$password = "";
-$dbname = "testdb";
-
-
-$con = mysqli_connect( $servername,$username ,$password, $dbname );
-
-if($con)
-{
-    echo " connected ";
-
-}
-else
-{
-
-    die("failed".mysql_connect_error()) ;
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-echo "start <br> "  ;
-
-
-
-
-
-
-
-
-
-echo "<br>";
-for ($k=1; $k <10 ; $k++) { 
- 
-    echo $k; 
-echo "<br>";
-for ($l=1; $l <=$k ; $l++) { 
-  echo "  $l  ." ;
-}
-
-
-
-
-}
-
-echo"<br>";
-for ($i=1; $i < 10; $i++) { 
-  
-    echo " $i  .";
-
-    echo "<br>";
-    for ($j=9; $j >$i ; $j--) { 
-       echo " $j .";
-    }
-}
-
-
-
-error_reporting(0);
-
-
-
-
-
-
-?> 
-
 <section style="background:grey; text-align:center; padding:1rem; margin:1rem  ; border:1rem solid black; border-radius:1rem; "   >
 
 
-<form  action="" method="">
+<form  action="action.php" method="POST">
 <br><br>
 
 NAME<br><input type="text" placeholder="name" name="name"> <br><br>
@@ -107,23 +25,6 @@ AGE<br><input type="text" name="age" placeholder="age"/><br><br>
 </section>
 
 
-
-
-<?php
-
-
-
-$sn = $_GET ['name'];
-$rl=$_GET ['roll'];
-$ag = $_GET ['age'];
-$cl=$_GET['class'];
-
-
-echo $sn;
-
-
-
-?>
 
 
     
